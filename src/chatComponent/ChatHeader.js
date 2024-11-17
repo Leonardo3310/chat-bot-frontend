9845d34db7568a6197e7c3fe2648e8dd2249167e
“Leonardo”3310 <jl.rangel@uniandes.edu.co>
2024-11-17 14:34:51 -0500

    header

--- src/chatComponent/ChatHeader.js
+++ src/chatComponent/ChatHeader.js
@@ -3,7 +3,7 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 
 function ChatHeader() {
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ backgroundColor: "#6369ca" }}>
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Financhat
